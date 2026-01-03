883ca4b0b80bcd260b8567044e4fce860644e281
zhenia-chugaev <zhenia.chugaev@bk.ru>
2026-01-03 18:57:48 +0100

    Format types file

--- types/index.ts
+++ types/index.ts
@@ -1,39 +1,39 @@
-export type Dictionary = Record<string, string | number | boolean>;
+export type Dictionary = Record<string, string | number | boolean>
 
-export type FormTemplate = Record<string, string | number>;
+export type FormTemplate = Record<string, string | number>
 
 export type FormFieldAttributes = Dictionary & {
-  as?: 'input' | 'textarea';
-  label?: string;
-  labelHtml?: Dictionary;
-};
+  as?: 'input' | 'textarea'
+  label?: string
+  labelHtml?: Dictionary
+}
 
 type EncriptionTypes =
   | 'application/x-www-form-urlencoded'
   | 'multipart/form-data'
-  | 'text/plain';
+  | 'text/plain'
 
-type FormTargets = '_self' | '_blank' | '_parent' | '_top';
+type FormTargets = '_self' | '_blank' | '_parent' | '_top'
 
 interface FormSubmissionAttributes {
-  url?: string;
-  novalidate?: boolean;
-  target?: FormTargets;
+  url?: string
+  novalidate?: boolean
+  target?: FormTargets
 }
 
 type GetFormAttributes = FormSubmissionAttributes & {
-  method: 'get';
-};
+  method: 'get'
+}
 
 type PostFormAttributes = FormSubmissionAttributes & {
-  method?: 'post';
-  enctype?: EncriptionTypes;
-};
+  method?: 'post'
+  enctype?: EncriptionTypes
+}
 
-export type FormAttributes = GetFormAttributes | PostFormAttributes;
+export type FormAttributes = GetFormAttributes | PostFormAttributes
 
-export type Formats = 'html';
+export type Formats = 'html'
 
 export interface FormGeneratorConfig {
-  format?: Formats;
+  format?: Formats
 }
